b918e38deb8c238741397a8f3d654df465307bec
AdrieanKhisbe <adriean.khisbe@live.fr>
2021-04-21 23:17:10 +0200

    Test setup adjustment :test_tube:

--- test/wait-a-minute.integration.test.js
+++ test/wait-a-minute.integration.test.js
@@ -4,11 +4,7 @@ const test = require('ava');
 
 const WAM_CLI = path.join(path.dirname(__dirname), 'bin', 'wait-a-minute');
 
-test.cb('wait-a-minute cli stdin', t => {
-  childProcess.exec(`${WAM_CLI} <<< ''`, {shell: true}, (err, stdout, stderr) => {
-    t.is(err, null);
-    t.is(stderr.trim(), '');
-    t.is(stdout.trim(), "Ctrl-D when you're okay");
-    t.end();
-  });
+test('wait-a-minute cli stdin', t => {
+  const stdout = childProcess.execSync(WAM_CLI, {encoding: 'utf-8'});
+  t.is(stdout.trim(), "Ctrl-D when you're okay");
 });
