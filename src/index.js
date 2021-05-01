c433997864d6b80afd4c2651b0f7517e32131caf
AdrieanKhisbe <adriean.khisbe@live.fr>
2021-05-01 22:20:27 +0200

    Full JS minimal reimplementation using readline :hammer_and_wrench:

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-module.exports = 'TODO';
+const readline = require('readline');
+
+const main = async process => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  });
+
+  rl.on('line', line => {
+    // noop
+  });
+
+  rl.on('close', () => {
+    console.log('Wait is now over');
+  });
+};
+
+module.exports = {main};
--- test/wait-a-minute.integration.test.js
+++ test/wait-a-minute.integration.test.js
@@ -6,5 +6,5 @@ const WAM_CLI = path.join(path.dirname(__dirname), 'bin', 'wait-a-minute');
 
 test('wait-a-minute cli stdin', t => {
   const stdout = childProcess.execSync(WAM_CLI, {encoding: 'utf-8'});
-  t.is(stdout.trim(), "Ctrl-D when you're okay");
+  t.is(stdout.trim(), "Ctrl-D when you're okay\nWait is now over");
 });
