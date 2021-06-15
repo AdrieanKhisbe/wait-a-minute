99cf5f1a9d3dc7496dea74bd7a0daf7ba78b3c46
AdrieanKhisbe <adriean.khisbe@live.fr>
2021-06-15 09:58:06 +0200

    Add minimal spinner with chalk and ora :cyclone:

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const readline = require('readline');
+const ora = require('ora');
+const c = require('chalk');
 
 const main = async process => {
   const rl = readline.createInterface({
@@ -6,13 +8,16 @@ const main = async process => {
     output: process.stdout,
     terminal: false
   });
+  const spinner = ora(
+    `Awaiting for ${c.bold.green('confirmation')} (${c.dim('Ctrl-D')}) to release focus`
+  ).start();
 
   rl.on('line', line => {
     // noop
   });
 
   rl.on('close', () => {
-    console.log('Wait is now over');
+    spinner.succeed(`Wait is now ${c.bold('over')}!`);
   });
 };
 
