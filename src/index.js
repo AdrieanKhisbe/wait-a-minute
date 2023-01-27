a4915a95fa697a55d2fcb820aaea8b71499c2495
AdrieanKhisbe <adriean.khisbe@live.fr>
2023-01-27 15:55:50 +0100

    Set up basic signal confirm system :satellite:

--- src/index.js
+++ src/index.js
@@ -18,20 +18,36 @@ const setConfirm = cb => {
   rl.on('line', line => null /* noop (so far) */);
   // TODO: validate word? (combine with once)
 
-  rl.on('close', () => cb());
+  rl.on('close', () => cb('confirm'));
 };
 
 const setDuration = (spinner, duration, cb) => {
   const delay = toMillis(String(duration));
-  setTimeout(cb, delay);
+  setTimeout(cb, delay, 'timeout');
   // TODO maybe, pause?
   // TODO: maybe interval update
+};
+
+const setSignal = async (spinner, process, cb) => {
+  spinner.info(`Signal ${c.dim('SIGUSR2')} to ${c.bold(process.pid)} to pursue`);
+  // SIGCONT? // SIGUSR1 reserved for node debugger
+
+  // TODO: pid could be write to file. (and eventual scope/alias be provided)
+  const hanger = setInterval(_.noop, 2000); // this to ensure process is hanging in there
+  process.on('SIGUSR2', () => {
+    cb('signal received');
+    clearInterval(hanger);
+  });
 };
 
 const argsParser = yargs().options({
   spinner: {
     type: 'boolean',
     default: true
+  },
+  signal: {
+    type: 'boolean',
+    default: false
   },
   color: {
     default: 'green'
@@ -53,7 +69,15 @@ const main = async process => {
   const args = argsParser.parse(process.argv.slice(2));
 
   const spinner = getSpinner(args).start();
-  const complete = () => spinner.succeed(`Wait is now ${c.bold[args.color]('over')}!`);
+  const complete = reason =>
+    spinner.succeed(
+      `Wait is now ${c.bold[args.color]('over!')} (reason ${c.dim[args.color](reason)})`
+    );
+
+  if (args.signal) {
+    process.stdin.resume();
+    return setSignal(spinner, process, complete);
+  }
 
   if (_.isEmpty(args._)) setConfirm(complete);
   else setDuration(spinner, args._[0], complete);
