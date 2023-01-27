5e42c3d859451cbe875fe53945c8edfc1839d216
AdrieanKhisbe <adriean.khisbe@live.fr>
2023-01-27 14:30:44 +0100

    Start to restructure for more completions mode :hammer_and_wrench:

--- src/index.js
+++ src/index.js
@@ -4,31 +4,40 @@ const c = require('chalk');
 const yargs = require('yargs/yargs');
 // const {} = require('./utils');
 
-const argsParser = yargs().option('spinner', {
-  type: 'boolean',
-  default: true
-});
+const types = ['duration', 'when', 'confirm', 'signal'];
 
-const main = async process => {
+// First of // all
+
+const setConfirm = cb => {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false
   });
+  rl.on('line', line => null /* noop (so far) */);
+
+  rl.on('close', () => cb());
+};
+
+const argsParser = yargs().options({
+  spinner: {
+    type: 'boolean',
+    default: true
+  }
+});
+
+const getSpinner = args => {
+  return ora({
+    text: `Awaiting for ${c.bold.green('confirmation')} (${c.dim('Ctrl-D')}) to release focus:`,
+    isEnabled: args.spinner
+  });
+};
+
+const main = async process => {
   const args = argsParser.parse(process.argv.slice(2));
 
-  const spinner = ora({
-    text: `Awaiting for ${c.bold.green('confirmation')} (${c.dim('Ctrl-D')}) to release focus`,
-    isEnabled: args.spinner
-  }).start();
-
-  rl.on('line', line => {
-    // noop (so far)
-  });
-
-  rl.on('close', () => {
-    spinner.succeed(`Wait is now ${c.bold('over')}!`);
-  });
+  const spinner = getSpinner(args).start();
+  setConfirm(() => spinner.succeed(`Wait is now ${c.bold('over')}!`));
 };
 
 module.exports = {main};
